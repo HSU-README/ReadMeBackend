5a3826dcfb04d395ce7d2dd417b4ee8ed5d179af
JGeun <pjk8926@gmail.com>
2022-02-20 03:04:32 +0900

    initial commit

---
+++ src/main/java/hsu/readme/ReadmeApplication.java
@@ -0,0 +1,13 @@
+package hsu.readme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hsu/readme/ReadmeApplicationTests.java
@@ -0,0 +1,13 @@
+package hsu.readme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
