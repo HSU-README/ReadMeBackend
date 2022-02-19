65e0e3f798e5650b7deed9981cf2df1f3d5b0cf3
JGeun <pjk8926@gmail.com>
2022-02-20 03:28:52 +0900

    add testcontroller

---
+++ src/main/java/hsu/readme/TestController.java
@@ -0,0 +1,13 @@
+package hsu.readme;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "test 성공했습니다";
+    }
+}
