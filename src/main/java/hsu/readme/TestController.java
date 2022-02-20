fab70743dd86cf1d8571e1d225500b02b678f863
JGeun <pjk8926@gmail.com>
2022-02-20 13:27:19 +0900

    make heroku db testcontroller

--- src/main/java/hsu/readme/TestController.java
+++ src/main/java/hsu/readme/TestController.java
@@ -1,13 +1,27 @@
 package hsu.readme;
 
+import hsu.readme.Repository.MemberRepository;
+import hsu.readme.domain.Member;
+import hsu.readme.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class TestController {
+
+    private final MemberService memberService;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/test")
     public String test(){
-        return "test 성공했습니다";
+        Member member = new Member();
+        member.setName("Jgeun");
+        Long savedId = memberService.join(member);
+        Member savedMember = memberRepository.findOne(savedId);
+
+        return "memberService: " + savedId + " name: " + member.getName() + "\n " +
+                "memberRepository:  " + savedMember.getId() + " name: " +savedMember.getName();
     }
 }
