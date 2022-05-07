27604a3e448a027c9aa0f67add089c05e310631c
JGeun <pjk8926@gmail.com>
2022-05-07 23:06:35 +0900

    allowedMethods PUT

--- src/main/java/hsu/readme/api/document/DocApiController.java
+++ src/main/java/hsu/readme/api/document/DocApiController.java
@@ -10,25 +10,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
+        import java.util.stream.Collectors;
 
-import static hsu.readme.api.ResponseMessage.DOC_INFO_SUCCESS;
-import static hsu.readme.api.ResponseMessage.HOME_INFO_SUCCESS;
+        import static hsu.readme.api.ResponseMessage.DOC_INFO_SUCCESS;
+        import static hsu.readme.api.ResponseMessage.HOME_INFO_SUCCESS;
 
-@RestController
-@RequiredArgsConstructor
-public class DocApiController {
+        @RestController
+        @RequiredArgsConstructor
+        public class DocApiController {
 
-    private final DocumentService documentService;
+        private final DocumentService documentService;
 
-    @GetMapping("/api/v1/doc/{id}/preview")
-    public Response docPreviewV1(@PathVariable Long id){ //doc id 보내줌
-        Document document = documentService.findDocumentWithMember(id);
-        return Response.response("S200", DOC_INFO_SUCCESS, new DocInfoDto(document));
-    }
+        @GetMapping("/api/v1/doc/{id}/preview")
+        public Response docPreviewV1(@PathVariable Long id){ //doc id 보내줌
+            Document document = documentService.findDocumentWithMember(id);
+            return Response.response("S200", DOC_INFO_SUCCESS, new DocInfoDto(document));
+        }
 
-    @GetMapping("/api/v1/doc/{id}")
-    public Response docInfoV1(@PathVariable Long id){
+        @GetMapping("/api/v1/doc/{id}")
+        public Response docInfoV1(@PathVariable Long id){
         Document document = documentService.findOne(id);
         return Response.response("S200", DOC_INFO_SUCCESS, new DocInfoDto(document));
     }
--- src/main/java/hsu/readme/config/WebConfig.java
+++ src/main/java/hsu/readme/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
         .allowedOrigins("*")
-        .allowedMethods("GET", "POST")
+        .allowedMethods("GET", "POST", "PUT")
         .maxAge(3000);
     }
 }
