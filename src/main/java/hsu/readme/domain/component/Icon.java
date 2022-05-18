7669fd03cd08f47aa02b8cddec9fab579e66e340
JGeun <pjk8926@gmail.com>
2022-05-18 22:35:02 +0900

    image url, icon url attach @lob

--- src/main/java/hsu/readme/domain/component/Icon.java
+++ src/main/java/hsu/readme/domain/component/Icon.java
@@ -5,11 +5,14 @@ import lombok.Setter;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.Lob;
 
 @Entity
 @Getter
 @Setter
 @DiscriminatorValue("icon")
 public class Icon extends Component{
+
+    @Lob
     private String iconUrl;
 }
--- src/main/java/hsu/readme/domain/component/Image.java
+++ src/main/java/hsu/readme/domain/component/Image.java
@@ -5,11 +5,14 @@ import lombok.Setter;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.Lob;
 
 @Entity
 @Getter
 @Setter
 @DiscriminatorValue("image")
 public class Image extends Component{
+
+    @Lob
     private String imgUrl;
 }
